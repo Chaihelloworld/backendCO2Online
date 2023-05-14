41d21f9e275bc1fffe7313f65ab97e501b898dbb
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-05-15 02:40:59 +0700

    update db

--- database.js
+++ database.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 
 
 var conn = mysql.createConnection({
-  // host: process.env.IP, // Replace with your host name
-  // user: process.env.USERNAME,      // Replace with your database username
-  // password: process.env.PASSWORD,      // Replace with your database password
-  // database: process.env.DB // // Replace with your database Name
-  host:'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: '',      // Replace with your database password
-  database: 'onlineshoppingco2' // // Replace with your database Name
+  host: process.env.IP, // Replace with your host name
+  user: process.env.USERNAME,      // Replace with your database username
+  password: process.env.PASSWORD,      // Replace with your database password
+  database: process.env.DB // // Replace with your database Name
+  // host:'localhost', // Replace with your host name
+  // user: 'root',      // Replace with your database username
+  // password: '',      // Replace with your database password
+  // database: 'onlineshoppingco2' // // Replace with your database Name
   // database: 'resource_information' // // Replace with your database Name
 }); 
  
