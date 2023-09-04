92428b1adfd8229174c7b2ef643bcfac00c00391
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-09-05 03:01:20 +0700

    update

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const indexRouter = require("./router.js");
-const dbConnection = require("./database");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const indexRouter = require('./router.js');
+const dbConnection = require('./database');
 
 const app = express();
-
+ 
 app.use(express.json());
-
+ 
 app.use(bodyParser.json());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
+ 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
 app.use(cors());
-
-app.use("/api", indexRouter);
-app.get("/", (req, res) => res.json("api is connecting"));
+ 
+app.use('/api', indexRouter);
+app.get('/',(req,res) => res.json('api is connecting'))
 // Handling Errors
 app.use((err, req, res, next) => {
-  // console.log(err);
-  err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal Server Error";
-  res.status(err.statusCode).json({
-    message: err.message,
-  });
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
 });
-const port = 8080; // Use a port number above 1024
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(5000,() => console.log('Server is running '));
