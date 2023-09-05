a6d962bbd1116dcf5e750ed9bc1fd5d315a4ddc0
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-09-05 22:00:10 +0700

    edit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
  
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://api.carbon-greentravel.com'); // Allow requests from your frontend
+  res.header('Access-Control-Allow-Origin', 'https://carbon-greentravel.com'); // Allow requests from your frontend
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
