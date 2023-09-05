8787fca3d1d4ca8c8578989f37f095a9e6c19b73
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-09-05 22:31:56 +0700

    update

--- router.js
+++ router.js
@@ -457,10 +457,10 @@ router.get("/cart_list", (req, res) => {
     Max(cart.id),
     users.name AS user_name,
     products.name AS product_name,
-    SUM(products.CO2*cart.count) AS total_CO2,
+    CAST(SUM(products.CO2*cart.count) AS DECIMAL(10, 2)) AS total_CO2,
   products.CO2 AS total_CO2_def,
     MAX(products.image) AS image, -- Assuming the image is the same for duplicates
-    SUM(cart.count) AS total_cart_count,
+    CAST(SUM(cart.count) AS INT) AS total_cart_count,
     MAX(cart.active) AS active -- Assuming active remains the same for duplicates
 FROM
     cart
