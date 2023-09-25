0b6f8e519832184fa488c6a0af866413e916fe09
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-09-25 21:42:23 +0700

    test fix

--- database.js
+++ database.js
@@ -1,22 +1,42 @@
-var mysql = require('mysql2');
-require('dotenv').config()
+// var mysql = require('mysql2');
+// require('dotenv').config()
 
 
-var conn = mysql.createConnection({
-  host: process.env.IP, // Replace with your host name
-  user: process.env.USERNAME,      // Replace with your database username
-  password: process.env.PASSWORD,      // Replace with your database password
-  database: process.env.DB // // Replace with your database Name
-  // host:'18.142.185.100', // Replace with your host name
-  // user: 'chine',      // Replace with your database username
-  // password: 'Gold26dg6pco*',      // Replace with your database password
-  // database: 'onlineshoppingco2' // // Replace with your database Name
-  // database: 'resource_information' // // Replace with your database Name
-}); 
+// var conn = mysql.createConnection({
+//   host: process.env.IP, // Replace with your host name
+//   user: process.env.USERNAME,      // Replace with your database username
+//   password: process.env.PASSWORD,      // Replace with your database password
+//   database: process.env.DB // // Replace with your database Name
+// }); 
  
  
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log('Database is connected successfully !');
-});
-module.exports = conn;
+// conn.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Database is connected successfully !');
+// });
+// module.exports = conn;
+
+module.exports = function () {
+
+  let mysql = require('mysql2')
+
+  //Establish Connection to the DB
+  let connection = mysql.createConnection({
+    host: process.env.IP, // Replace with your host name
+    user: process.env.USERNAME,      // Replace with your database username
+    password: process.env.PASSWORD,      // Replace with your database password
+    database: process.env.DB // // Replace with your database Name
+  });
+
+  //Instantiate the connection
+  connection.connect(function (err) {
+      if (err) {
+          console.log(`connectionRequest Failed ${err.stack}`)
+      } else {
+          console.log(`DB connectionRequest Successful ${connection.threadId}`)
+      }
+  });
+
+  //return connection object
+  return connection
+}
--- router.js
+++ router.js
@@ -18,16 +18,17 @@ const cookieSession = require("cookie-session");
 const { json } = require("body-parser");
 
 router.post("/register", signupValidation, async (req, res, next) => {
+  let connection = db()
   // let db = await connectDB();
-  db.query(
-    `SELECT * FROM users WHERE LOWER(email) = LOWER(${db.escape(
+  connection.query(
+    `SELECT * FROM users WHERE LOWER(email) = LOWER(${connection.escape(
       req.body.email
     )});`,
     (err, result) => {
       console.log(11111)
       console.log(result)
       if (result.length) {
-         
+        connection.destroy()
         return res.status(409).send({
           msg: "This user is already in use!",
         });
@@ -35,27 +36,27 @@ router.post("/register", signupValidation, async (req, res, next) => {
         // username is available
         bcrypt.hash(req.body.password, 10, (err, hash) => {
           if (err) {
-             
+            connection.destroy()
             return res.status(500).send({
               msg: err,
             });
           } else {
             // has hashed pw => add to database
-            db.query(
+            connection.query(
               `INSERT INTO users (name, email, password, role) VALUES ('${
                 req.body.name
-              }', ${db.escape(req.body.email)}, ${db.escape(hash)},'${
+              }', ${connection.escape(req.body.email)}, ${connection.escape(hash)},'${
                 req.body.role
               }')`,
               (err, result) => {
                 if (err) {
                   throw err;
-                   
+                  connection.destroy()
                   return res.status(400).send({
                     msg: err,
                   });
                 }
-                 
+                connection.destroy()
                 return res.status(201).send({
                   msg: "The user has been registerd with us!",
                 });
@@ -77,18 +78,20 @@ app.use(
 );
 
 router.post("/login", loginValidation, (req, res, next) => {
-  db.query(
-    `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
+  let connection = db()
+  connection.query(
+    `SELECT * FROM users WHERE email = ${connection.escape(req.body.email)};`,
     (err, result) => {
       // user does not exists
       if (err) {
         throw err;
+        connection.destroy()
         return res.status(400).send({
           msg: err,
         });
       }
       if (!result.length) {
-         
+        connection.destroy()
         return res.status(401).send({
           msg: "Email or password is incorrect!",
         });
@@ -116,14 +119,14 @@ router.post("/login", loginValidation, (req, res, next) => {
             // );
             // req.session.isLoggedIn = true;
             // req.session.userID = rows[0].id;
-             
+            connection.destroy()
             return res.status(200).send({
               msg: "Logged in!",
               token,
               user: result[0],
             });
           }
-           
+          connection.destroy()
           return res.status(401).send({
             msg: "Username or password is incorrect!",
           });
@@ -133,11 +136,14 @@ router.post("/login", loginValidation, (req, res, next) => {
   );
 });
 router.get("/get-user", signupValidation, (req, res, next) => {
+  let connection = db()
+
   if (
     !req.headers.authorization ||
     !req.headers.authorization.startsWith("Bearer") ||
     !req.headers.authorization.split(" ")[1]
   ) {
+    connection.destroy()
     return res.status(422).json({
       message: "Please provide the token",
     });
@@ -145,12 +151,12 @@ router.get("/get-user", signupValidation, (req, res, next) => {
   const theToken = req.headers.authorization.split(" ")[1];
   const decoded = jwt.verify(theToken, "the-super-strong-secrect");
   if(decoded.id){
-    db.query(
+    connection.query(
       "SELECT * FROM users where id=?",
       decoded.id,
       function (error, results, fields) {
         if (error) throw error;
-         
+        connection.destroy()
         return res.send({
           error: false,
           data: results[0],
@@ -169,14 +175,15 @@ router.get("/logout", (req, res) => {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 router.post("/create_products", (req, res) => {
+  let connection = db()
   const { name, description, image, CO2, type_products, category_id } =
     req.body;
 
   const product = { name, description, image, CO2, type_products, category_id };
   console.log(product);
-  db.query("INSERT INTO products SET ?", product, (error, results, fields) => {
+  connection.query("INSERT INTO products SET ?", product, (error, results, fields) => {
     if (error) throw error;
-     
+    connection.destroy()
     res.send({
       success: true,
       message: "Product added successfully.",
@@ -184,6 +191,7 @@ router.post("/create_products", (req, res) => {
   });
 });
 router.get("/products_list", (req, res) => {
+  let connection = db()
   const { category_id, name, page, perPage } = req.query;
   console.log("name", name);
   let query = `SELECT p.*, c.name AS category_name
@@ -218,7 +226,7 @@ router.get("/products_list", (req, res) => {
   // }
 
   // Execute count query to get total number of results
-  db.query(countQuery, countParams, (error, countResults, fields) => {
+  connection.query(countQuery, countParams, (error, countResults, fields) => {
     if (error) throw error;
 
     const totalResults = countResults[0].count;
@@ -233,14 +241,14 @@ router.get("/products_list", (req, res) => {
     db.query(query, params, (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
-         
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
-         
+        connection.destroy()
         res.send({
           success: true,
           data: results,
@@ -288,17 +296,18 @@ router.get("/products_list", (req, res) => {
 //   });
 // });
 router.get("/categories", (req, res) => {
-  db.query("SELECT * FROM product_categories", (error, results, fields) => {
+  let connection = db()
+  connection.query("SELECT * FROM product_categories", (error, results, fields) => {
     if (error) throw error;
     if (results.length === 0) {
-       
+      connection.destroy()
       res.send({
         success: false,
         data: [],
         message: "Fetch error.",
       });
     } else {
-       
+      connection.destroy()
       res.send({
         success: true,
         data: results,
@@ -308,8 +317,9 @@ router.get("/categories", (req, res) => {
   });
 });
 router.get("/info_product", (req, res) => {
+  let connection = db()
   req.query.id;
-  db.query(
+  connection.query(
     `SELECT p.*, c.name AS category_name
   FROM products p
   JOIN product_categories c ON p.category_id = c.id
@@ -317,14 +327,14 @@ router.get("/info_product", (req, res) => {
     (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
-         
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
-         
+        connection.destroy()
         res.send({
           success: true,
           data: results,
@@ -336,19 +346,20 @@ router.get("/info_product", (req, res) => {
 });
 
 router.delete("/delete_product", (req, res, next) => {
+  let connection = db()
   const id = req.query.id;
   console.log(id);
 
-  db.query(`DELETE FROM products WHERE id = '${id}'`, (err, result) => {
+  connection.query(`DELETE FROM products WHERE id = '${id}'`, (err, result) => {
     if (result && result.affectedRows > 0) {
       // <-- Check if at least one row was deleted
-       
+      connection.destroy()
       res.send({
         success: true,
         message: "Delete Successfully.",
       });
     } else {
-       
+      connection.destroy()
       res.send({
         success: false,
         message: "Delete Unsuccessful.",
@@ -357,11 +368,12 @@ router.delete("/delete_product", (req, res, next) => {
   });
 });
 router.get("/check_product", (req, res) => {
+  let connection = db()
   const value_co2 = req.query.value_co2;
   const type = req.query.type;
   const id = req.query.id;
 
-  db.query(
+  connection.query(
     `SELECT *
   FROM products
   WHERE CO2 BETWEEN ${value_co2}-5 AND ${value_co2}
@@ -371,14 +383,14 @@ router.get("/check_product", (req, res) => {
     (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
-         
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
-         
+        connection.destroy()
         res.send({
           success: true,
           data: results,
@@ -395,24 +407,25 @@ SELECT c.username AS user_name, c.point, p.name AS product_name, p.CO2, p.image,
 */
 }
 router.get("/select_product", (req, res) => {
+  let connection = db()
   const in_id = req.query.in_id;
   console.log(in_id);
 
-  db.query(
+  connection.query(
     `SELECT name,CO2,image
     FROM products
     WHERE id IN (${in_id});`,
     (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
-         
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
-         
+        connection.destroy()
         res.send({
           success: true,
           data: results,
@@ -424,15 +437,16 @@ router.get("/select_product", (req, res) => {
 });
 // INSERT INTO list_productbyuser (userId, product_id) VALUES (1, 100), (1, 101), (2, 102);
 router.post("/create_listitem", (req, res) => {
+  let connection = db()
   const { userId, product_id } = req.body;
 
   const product = { userId, product_id };
-  db.query(
+  connection.query(
     "INSERT INTO list_productbyuser SET ?",
     product,
     (error, results, fields) => {
       if (error) throw error;
-       
+      connection.destroy()
       res.send({
         success: true,
         message: "Product added successfully.",
@@ -442,12 +456,13 @@ router.post("/create_listitem", (req, res) => {
 });
 
 router.post("/create_cart", (req, res) => {
+  let connection = db()
   const { user_id, product_id, count, active } = req.body;
 
   const product = { user_id, product_id, count, active };
-  db.query("INSERT INTO cart SET ?", product, (error, results, fields) => {
+  connection.query("INSERT INTO cart SET ?", product, (error, results, fields) => {
     if (error) throw error;
-     
+    connection.destroy()
     res.send({
       success: true,
       message: "Product added successfully.",
@@ -456,20 +471,21 @@ router.post("/create_cart", (req, res) => {
 });
 
 router.get("/cart_count", (req, res) => {
+  let connection = db()
   req.query.user_id;
-  db.query(
+  connection.query(
     `SELECT SUM(count) AS total_count FROM cart WHERE active = 1 AND user_id = ${req.query.user_id};`,
     (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
-         
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
-         
+        connection.destroy()
         res.send({
           success: true,
           data: results,
@@ -481,9 +497,11 @@ router.get("/cart_count", (req, res) => {
 });
 
 router.get("/cart_list", (req, res) => {
+  let connection = db()
   const userId = req.query.user_id; // Store user_id in a variable
 
   if (!userId) {
+    connection.destroy()
     // If user_id is not provided in the query parameters, return an error response.
     return res.status(400).json({
       success: false,
@@ -492,7 +510,7 @@ router.get("/cart_list", (req, res) => {
     });
   }
 
-  db.query(
+  connection.query(
     `SELECT
       MAX(cart.id) AS id,
       users.name AS user_name,
@@ -516,6 +534,7 @@ router.get("/cart_list", (req, res) => {
     (error, results, fields) => {
       if (error) {
         console.error(error);
+        connection.destroy()
         return res.status(500).json({
           success: false,
           data: [],
@@ -524,13 +543,14 @@ router.get("/cart_list", (req, res) => {
       }
 
       if (results.length === 0) {
+        connection.destroy()
         return res.status(404).json({
           success: false,
           data: [],
           message: "Cart list not found for the specified user_id.",
         });
       }
-
+      connection.destroy()
       res.status(200).json({
         success: true,
         data: results,
@@ -542,10 +562,11 @@ router.get("/cart_list", (req, res) => {
 
 
 router.put("/update_cart/:id", (req, res) => {
+  let connection = db()
   const cartId = req.params.id;
   const { total_cart_count } = req.body; // Assuming you're sending this data in the request body
   console.log(req.body, req.params);
-  db.query(
+  connection.query(
     `UPDATE cart
      SET count = ?
      WHERE id = ?`,
@@ -553,12 +574,12 @@ router.put("/update_cart/:id", (req, res) => {
     (error, results, fields) => {
       if (error) {
         console.error("Error updating cart item:", error);
-         
+        connection.destroy()
         res
           .status(500)
           .send({ success: false, message: "Error updating cart item." });
       } else {
-         
+        connection.destroy()
         res.send({ success: true, message: "Cart item updated successfully." });
       }
     }
@@ -566,11 +587,12 @@ router.put("/update_cart/:id", (req, res) => {
 });
 
 router.post("/update_cart_active", (req, res) => {
+  let connection = db()
   console.log(req.body);
   const updateData = req.body.updatedCartItems; // Assuming the request body contains the update array
   const user_id = req.body.user_id;
   if (!updateData || !Array.isArray(updateData)) {
-     
+    connection.destroy()
     res.status(400).json({
       success: false,
       message: "Invalid update data format.",
@@ -583,8 +605,8 @@ router.post("/update_cart_active", (req, res) => {
       const id = item.id;
       const totalCartCount = item.total_cart_count;
       const user_id = item.user_id;
-
-      db.query(
+      let connection = db()
+      connection.query(
         `UPDATE cart
         SET active = ${0}  AND id = ${id} WHERE user_id = ${user_id}`,
         (error, results, fields) => {
@@ -600,13 +622,14 @@ router.post("/update_cart_active", (req, res) => {
 
   Promise.all(updatePromises)
     .then(() => {
-       
+      connection.destroy()
       res.json({
         success: true,
         message: "Cart active status updated successfully.",
       });
     })
     .catch((error) => {
+      connection.destroy()
       res.status(500).json({
         success: false,
         message: "Error updating cart active status.",
@@ -616,31 +639,37 @@ router.post("/update_cart_active", (req, res) => {
 });
 
 router.post("/historyCart", (req, res) => {
+  let connection = db()
   const { name, dataset, total_CO2, user_id } = req.body.param;
 
   const insertQuery = `INSERT INTO historycart (name, dataset, total_CO2, user_id) VALUES (?, ?, ?, ?)`;
 
-  db.query(insertQuery, [name, dataset, total_CO2, user_id], (err, result) => {
+  connection.query(insertQuery, [name, dataset, total_CO2, user_id], (err, result) => {
     if (err) {
       console.error(err);
+      connection.destroy()
       res.status(500).send("Error inserting data into historyCart");
     } else {
+      connection.destroy()
       res.status(201).send("Data inserted into historyCart");
     }
   });
 });
 
 router.delete("/delete_Cart", (req, res, next) => {
+  let connection = db()
   const userId = req.query.user_id;
   console.log(userId);
-  db.query(`DELETE FROM cart WHERE user_id = ${userId}`, (err, result) => {
+  connection.query(`DELETE FROM cart WHERE user_id = ${userId}`, (err, result) => {
     if (result && result.affectedRows > 0) {
       // <-- Check if at least one row was deleted
+      connection.destroy()
       res.send({
         success: true,
         message: "Delete Successfully.",
       });
     } else {
+      connection.destroy()
       res.send({
         success: false,
         message: "Delete Unsuccessful.",
@@ -649,18 +678,21 @@ router.delete("/delete_Cart", (req, res, next) => {
   });
 });
 router.get("/historycart", (req, res) => {
+  let connection = db()
   req.query.user_id;
-  db.query(
+  connection.query(
     `SELECT * FROM historycart WHERE user_id = ${req.query.user_id};`,
     (error, results, fields) => {
       if (error) throw error;
       if (results.length === 0) {
+        connection.destroy()
         res.send({
           success: false,
           data: [],
           message: "Fetch error.",
         });
       } else {
+        connection.destroy()
         res.send({
           success: true,
           data: results,
